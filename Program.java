e4750904951b5db98833f5e41919bb8e4ff25140
unknown <inf132241@lab-44-12.cs.put.poznan.pl>
2018-10-30 15:58:00 +0100

    Pierwsza

--- Program.java
+++ Program.java
@@ -1,8 +1,10 @@
 public class MainClass {
 	int a = 0;
 	public static void main(String[] args) {
+		//C1
 		int b = 1;
 		System.out.println("Abcd");
+		int c = 2;
 		System.out.println("1234");
 	}
 }
